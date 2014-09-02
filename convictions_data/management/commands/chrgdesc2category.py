ddc8c3f5c8c961893a411988a93cd47c843bbd31
Brian Peterson <bepetersn@gmail.com>
2014-09-02 12:13:12 -0500

    get chrgdesc2category command to reveal real multiple possible categories given a chrgdesc

--- convictions_data/management/commands/chrgdesc2category.py
+++ convictions_data/management/commands/chrgdesc2category.py
@@ -1,29 +1,13 @@
-import logging
+from __future__ import division
 
 from django.core.management.base import BaseCommand
-from django.db import transaction
 
 from convictions_data.models import Conviction
 from convictions_data.statute import (get_iucr, IUCRLookupError,
     ILCSLookupError, StatuteFormatError)
 
-from itertools import chain
 import json
 
-
-def update_or_create(dict, chrgdesc, category):
-    if category:
-        try:
-            categories = dict[chrgdesc]
-            if category not in categories:
-                # uncomment next line to see multiple IUCR categories acrue
-                #import pdb; pdb.set_trace()
-                dict[chrgdesc].append(category)
-        except KeyError:
-            dict[chrgdesc] = [category]
-    else:
-        # warn if there's no IUCR category for this conviction
-        assert False
 
 FIRST_TIME = True
 
@@ -36,75 +20,103 @@ def true_once():
         return False
 
 
+def div(x, y):
+    try:
+        return round((x / y), 4) * 100
+    except ZeroDivisionError:
+        return 0
+
+
+def update_or_create(dict, chrgdesc, new_categories):
+    try:
+        present_categories = dict[chrgdesc]
+        for c in new_categories:
+            if c not in present_categories:
+                dict[chrgdesc].append(c)
+    except KeyError:
+        dict[chrgdesc] = new_categories
+
+
 class Command(BaseCommand):
-    help = "Map charge descriptions to iucr categories."
+    help = \
+        """  
+        Try to generate, as close as possible, 
+        a one-to-one mapping between charge 
+        descriptions and iucr categories.
+        """
 
     def handle(self, *args, **options):
 
         print('inside the command')
 
         chrgdesc_to_category = {}
+        hit = 0
 
-        for conviction in Conviction.objects.all():
+        convictions = Conviction.objects.all()
+        total = convictions.count()
+
+        for i, conviction in enumerate(convictions):
 
             if true_once():
                 print('inside the iteration')
 
             chrgdesc = conviction.final_chrgdesc
             category = conviction.iucr_category
-
-            case_number = conviction.case_number
             statute = conviction.final_statute
-            chrgdispdate = conviction.chrgdispdate
 
-            try:
-                update_or_create(chrgdesc_to_category, chrgdesc, category)
-            except AssertionError:
-                
-                #print ('NO IUCR found for conviction: {}').format(conviction)
-                offense_tuples = set()
-                for conviction in Conviction.objects.filter(final_chrgdesc=chrgdesc):
-                    try:
-                        offense_tuples.add(tuple(get_iucr(conviction.final_statute)))
-                    except IUCRLookupError:
-                        #print ("UNKNOWN CODE associated /w chrgdesc: {}").format(chrgdesc)
-                        pass
-                    except ILCSLookupError:
-                        #print ("UNKNOWN STATUTE associated /w chrgdesc: {}").format(chrgdesc)
-                        pass
-                    except StatuteFormatError:
-                        #print ("UNKNOWN FORAMT associated /w chrgdesc: {}").format(chrgdesc)
-                        pass
+            # if exactly one IUCR code / category is associated
+            # with this conviction, map it to the conviction's
+            # charge description;
 
-                if len(offense_tuples) == 1:
-                    #print 'SAME IUCRs from every conviction' 
-                    offenses = list(offense_tuples)[0]
-                    if len(set(o.offense_category for o in offenses)) == 1:
-                        #print 'SAME CATEGORY for all the IUCRs'
+            # also make sure that the category can be found in the crosswalk's 
+            # list of possible categories and is not just somehow in the database
+            if category and category not in [o.offense_category for o in get_iucr(statute)]:
+                category = ''
+
+            if category:
+                update_or_create(chrgdesc_to_category, chrgdesc, [category])
+                hit += 1
+
+            # otherwise, check if the conviction doesn't have an IUCR
+            # because multiple possible IUCRs matched the conviction's statute   
+            else:                
+                try:
+                    offenses = get_iucr(statute)
+                except IUCRLookupError:
+                    continue
+                except ILCSLookupError:
+                    continue
+                except StatuteFormatError:
+                    continue
+
+                if len(offenses) >= 1:
+                    # if so, check if all possible IUCRs associated with
+                    # that statute map to a single charge description;
+                    if len(set([o.offense_category for o in offenses])) == 1:
                         category = offenses[0].offense_category
-                        if category:
-                            #print('SUCCESS: All convictions /w this chrgdesc ({}) have the '
-                                #'same iucr category ({})').format(chrgdesc, category)
-                            update_or_create(chrgdesc_to_category, chrgdesc, category)
-                        else:
-                            #print 'something weird is going on'
-                            pass
-                    else:
-                        #print "Couldn't get an IUCR category for this"
-                        pass
 
-        print('num total: ', len(chrgdesc_to_category))
+                        update_or_create(chrgdesc_to_category, chrgdesc, [category])
+                        hit += 1
+                    else:
+                        categories = list(set([o.offense_category for o in offenses]))
+                        update_or_create(chrgdesc_to_category, chrgdesc, categories)
+
+            print "{}% one-to-one mapping".format(div(hit, i))
+
+        print 'num total: ', len(chrgdesc_to_category)
         print 'writing file with all'
         with open('chrgdesc_to_category__all.json', 'w') as f:
             json.dump(chrgdesc_to_category, f)
 
-        print 'sorting'
-        chrgdesc_to_category = {x: chrgdesc_to_category[x] for x in chrgdesc_to_category.keys() if len(chrgdesc_to_category[x]) > 1}
+        chrgdesc_to_category_multiples = {x: chrgdesc_to_category[x] for x in chrgdesc_to_category.keys() if len(chrgdesc_to_category[x]) > 1}
         
-        print('num with multiple: ', len(chrgdesc_to_category))
-        print 'writing mutliples file'
+        print 'num chrgdesc that map to multiple possible IUCR categories: ', len(chrgdesc_to_category_multiples)
+        print 'writing multiples file'
         with open('chrgdesc_to_category__multiples.json', 'w') as f:
-            json.dump(chrgdesc_to_category, f)
+            json.dump(chrgdesc_to_category_multiples, f)
+
+        print ('num convictions whose chrgdesc maps to multiple possible '
+              'IUCR categories: {}').format(total - hit)
 
         print 'done'
 
